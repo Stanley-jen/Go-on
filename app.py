d4c63938f4a8571b9c0262d13c330773ed0c95c9
Stanley Jen <stanley@Stanleys-MacBook-Pro.local>
2025-12-06 16:28:34 +0800

    Initial commit - Flask license server

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+VALID_LICENSES = {"TEST123": True}
+
+@app.route("/validate", methods=["POST"])
+def validate():
+    data = request.get_json()
+    key = data.get("license_key")
+    return jsonify({"valid": VALID_LICENSES.get(key, False)})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
